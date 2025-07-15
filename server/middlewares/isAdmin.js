5411b2458f4518eebe92b709d00b692dc49dc5f8
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-15 16:53:37 +0530

    Update isAdmin.js

--- server/middlewares/isAdmin.js
+++ server/middlewares/isAdmin.js
@@ -12,22 +12,12 @@ const protectAdmin = async (req, res, next) => {
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
-  const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
-  console.log("Decoded Token:", decoded);
-
-  const admin = await Admin.findById(decoded.id).select("-password");
-  if (!admin) {
-    console.warn("Admin not found for ID:", decoded.id);
-    return res.status(401).json({ message: "Admin not found" });
+    const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+    req.admin = await Admin.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
   }
-
-  req.admin = admin;
-  next();
-} catch (err) {
-  console.error("JWT Error:", err.name, err.message);
-  res.status(401).json({ message: "Invalid or expired token" });
-}
- 
 };
 
 module.exports = protectAdmin;

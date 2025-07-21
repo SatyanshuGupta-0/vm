27ab8820a47a974bb2b33e3f9c2de9bac3ec54c2
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-21 10:18:03 +0530

    Update VMAdminRoute.js

--- server/router/VMAdminRoute.js
+++ server/router/VMAdminRoute.js
@@ -8,8 +8,9 @@ const {
   getAdminProfile
 } = require("../controller/Admin.Controller");
 const protectAdmin = require("../middlewares/isAdmin");
+const authorizeRoles = require("../middlewares/roleAuth");
 
-router.post("/registers", registerAdmin);
+router.post("/registers",authorizeRoles("superadmin"), registerAdmin);
 router.post("/login", loginAdmin);
 router.get("/refresh-token", refreshToken);
 router.get("/logout", logoutAdmin);

89345fc53088cd93372ec8633bfbc51a319c842f
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-22 15:25:04 +0530

    Update VMWishlist.model.js

--- server/model/VMWishlist.model.js
+++ server/model/VMWishlist.model.js
@@ -11,6 +11,7 @@ const wishlistSchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
+    sizeId: String,
     productTitle: String,
     image: String,
     rating: Number,

efc7469f4bcc93da81ecc6110edc123817dcac12
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-23 21:18:12 +0530

    Update VMCartproduct.model.js

--- server/model/VMCartproduct.model.js
+++ server/model/VMCartproduct.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const CartProductSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Variant', required: false }, // add this
+  variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Variant', required: false },
+  sizeId: { type: mongoose.Schema.Types.ObjectId, required: false }, // add this
   quantity: { type: Number, required: true },
   // any other fields
 });

2039d5c4ea5cc3ee1b5bebeb3a8baa1dbb34541a
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-23 23:18:38 +0530

    Update VMCartRoute.js

--- server/router/VMCartRoute.js
+++ server/router/VMCartRoute.js
@@ -6,13 +6,16 @@ const {
   addToCartItemController,
   getCartItemController,
   updateCartItemQtyController,
-  deleteCartItemController
+  deleteCartItemController,
+  clearCartController
 } = require("../controller/cart.controller");
 
 // User-only routes
 router.post("/add", auth, addToCartItemController);               // Add item to cart
 router.get("/get", auth, getCartItemController);                  // Get user's cart items
 router.put("/update-qty", auth, updateCartItemQtyController);     // Update quantity
-router.delete("/delete-cart-item/:id", auth, deleteCartItemController); // Delete item
+router.delete("/delete-cart-item/:id", auth, deleteCartItemController);// Delete item
+router.delete("/clear", auth, clearCartController);
+
 
 module.exports = router;

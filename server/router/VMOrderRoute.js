086cf3461bc4cb8fa0816cea9e4458019765483d
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-25 09:07:33 +0530

    Update VMOrderRoute.js

--- server/router/VMOrderRoute.js
+++ server/router/VMOrderRoute.js
@@ -21,6 +21,6 @@ router.put("/cancel/:id", auth, cancelOrderController);
 router.get("/user-orders", auth, viewOrderController);
 
 // Admin: View all orders
-router.get("/all-orders",isAdmin("superadmin","admin"),   getAllOrdersController);
+router.get("/all-orders",isAdmin("superadmin","admin","shopkeeper","manager"),   getAllOrdersController);
 
 module.exports = router;

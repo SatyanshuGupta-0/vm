c3367c208517c14d50774ca79ff6e6dc08028da8
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-25 09:44:42 +0530

    Update isAdmin.js

--- server/middlewares/isAdmin.js
+++ server/middlewares/isAdmin.js
@@ -9,7 +9,7 @@ const isAdmin = (...allowedRoles) => {
 
       // ✅ Check if token exists and is a Bearer token
       if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "No token provided lol" });
+        return res.status(401).json({ message: "No token provided" });
       }
 
       const token = authHeader.split(" ")[1];

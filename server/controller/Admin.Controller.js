fab5220de90872c4548776a49947977808c763e5
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-25 21:34:39 +0530

    Update Admin.Controller.js

--- server/controller/Admin.Controller.js
+++ server/controller/Admin.Controller.js
@@ -8,8 +8,8 @@ const generateRefreshToken = require("../utils/generatedRefreshToken");
 const setRefreshTokenCookie = (res, token) => {
   res.cookie("refreshToken", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };

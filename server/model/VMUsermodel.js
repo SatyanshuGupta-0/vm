83f826607c5e77ac42a5aa99669d82e8bf9b5d17
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-27 13:36:53 +0530

    Update VMUsermodel.js

--- server/model/VMUsermodel.js
+++ server/model/VMUsermodel.js
@@ -37,10 +37,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
-    refresh_tokens: {
-  type: [String], // Array of strings
-  default: [],
+    refresh_token: {
+  type: String,
+  default: ""
 },
+
     last_login_date: {
         type: Date,
         default: ""

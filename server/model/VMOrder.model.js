2efd2b1a96dad60448de2ad29463bdf291bf91f4
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-07-28 09:35:11 +0530

    Update VMOrder.model.js

--- server/model/VMOrder.model.js
+++ server/model/VMOrder.model.js
@@ -70,7 +70,7 @@ const orderSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["placed", "shipped","out-for-delivery", "delivered", "cancelled"],
+    enum: ["placed", "shipped","out-for-delivery", "delivered", "cancelled","refundRequested"],
     default: "placed",
   },
   deliveredAt: Date,

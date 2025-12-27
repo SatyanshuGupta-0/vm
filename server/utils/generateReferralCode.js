9e8f6769f3a3e6ab76ccc3b4e786e2ba95b499a5
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-12-27 08:18:16 +0530

    Update generateReferralCode.js

--- server/utils/generateReferralCode.js
+++ server/utils/generateReferralCode.js
@@ -1,1 +1,9 @@
 
+function generateReferralCode(name = "") {
+  return (
+    name.substring(0, 4).toUpperCase() +
+    Math.floor(1000 + Math.random() * 9000)
+  );
+}
+
+module.exports = generateReferralCode;

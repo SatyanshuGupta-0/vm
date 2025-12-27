873fada0ed8f79995f65013e4b2525fa66a57f78
SatyanshuGupta-0 <satyanshugupta282002@gmail.com>
2025-12-27 14:47:50 +0530

    Update sendEmail.js

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -1,8 +1,22 @@
+// const sendEmail = require("./emailService");
+
+// const sendEmailFun = async (to, subject, text, html) => {
+//     const result = await sendEmail(to, subject, text, html);
+//     return result; // Simplified to return the boolean result directly
+// };
+
+// module.exports = sendEmailFun;
+
 const sendEmail = require("./emailService");
 
 const sendEmailFun = async (to, subject, text, html) => {
-    const result = await sendEmail(to, subject, text, html);
-    return result; // Simplified to return the boolean result directly
+  try {
+    await sendEmail(to, subject, text, html);
+    return true;
+  } catch (error) {
+    console.error("❌ Email Error:", error.message);
+    return false;
+  }
 };
 
 module.exports = sendEmailFun;
